b10c8752430b7e1031185d596c585f9e323957bc
Samuel <lee.eunseong7856@gmail.com>
2022-10-12 19:06:38 -0500

    commit tree fb9faaa19bd475c343bac2467e012afb993ac839

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const messages = [];
+function writeCards (name, event) {
+    console.log(name, event)
+    for (let x = 0; x < name.length; x++) {
+        messages.push(`Thank you, ${name[x]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+//writeCards(names);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--
+    }
+}
